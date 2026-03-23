b4404cac9ed5711eaca49bff10602351f2d7f434
Ivan <github@laodesign.es>
2026-03-23 22:59:12 +0000

    core: Add extra validations on load handlers

--- src/bot/loadResources.js
+++ src/bot/loadResources.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import i18next from '../i18n/index.js';
 import path from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 
@@ -8,7 +9,11 @@ const __dirname = path.dirname(__filename);
 export async function loadResources(dirName, fileName) {
   const resourceDir = path.join(__dirname, dirName);
 
-  const files = fs.readdirSync(resourceDir).filter((file) => file.endsWith(fileName));
+  // Sorting handlers files
+  const files = fs
+    .readdirSync(resourceDir)
+    .filter((file) => file.endsWith(fileName))
+    .sort();
 
   const resources = {};
 
@@ -19,7 +24,41 @@ export async function loadResources(dirName, fileName) {
     const module = await import(fileUrl);
     const resource = module.default;
 
-    resources[resource.name] = resource;
+    if (!resource || typeof resource !== 'object') {
+      // Resource is not an object
+      i18next.errorT('handlers.validations.resource', { handler_filename: fileUrl });
+      continue;
+    }
+
+    if (!resource.name || typeof resource.name !== 'string') {
+      // resource.name is not a string or not exists
+      i18next.errorT('handlers.validations.name', { fileUrl });
+      continue;
+    }
+
+    if (typeof resource.execute !== 'function') {
+      // resource.execute is not a function
+      i18next.errorT('handlers.validations.execute', { fileUrl });
+      continue;
+    }
+
+    // Normalize handler name
+    const handlerName = resource.name.toLowerCase().trim();
+
+    if (resources[handlerName]) {
+      i18next.errorT('handlers.already_exists', {
+        handler_name: handlerName,
+        handler_filename: fileUrl,
+        handler_filename_exists: resources[handlerName].__file,
+      });
+      continue;
+    }
+
+    resources[handlerName] = {
+      ...resource,
+      name: handlerName,
+      __file: fileUrl,
+    };
   }
 
   return resources;
