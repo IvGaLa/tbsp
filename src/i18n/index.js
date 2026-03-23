584ae6b3a775e16f586be8057bffc0905918c5e8
Ivan <github@laodesign.es>
2026-03-23 22:57:57 +0000

    feature: Extend i18next with custom method

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -57,4 +57,47 @@ await i18next
     interpolation: { escapeValue: false },
   });
 
+// Extending i18next to use console.log, console.error, etc... witch colors
+i18next.consoleT = (_msg, _options = {}, _type = null, _color = null) => {
+  const colors = {
+    reset: '\x1b[0m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    purple: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+  };
+
+  const methods = {
+    error: { method: 'error', color: colors[_color] || colors.red },
+    warn: { method: 'warn', color: colors[_color] || colors.yellow },
+    info: { method: 'log', color: colors[_color] || colors.blue },
+    default: { method: 'log', color: colors[_color] || colors.white },
+  };
+
+  const { method, color } = methods[_type] || methods.default;
+  const text = i18next.t(_msg, _options);
+
+  console[method](`${color}${text}${colors.reset}`);
+};
+
+// Access methods for 'error', 'warn', 'info', 'log'
+i18next.errorT = (_msg, _options = {}) => {
+  i18next.consoleT(_msg, _options, 'error');
+};
+
+i18next.warnT = (_msg, _options = {}) => {
+  i18next.consoleT(_msg, _options, 'warn');
+};
+
+i18next.infoT = (_msg, _options = {}) => {
+  i18next.consoleT(_msg, _options, 'info');
+};
+
+i18next.logT = (_msg, _options = {}) => {
+  i18next.consoleT(_msg, _options, 'log');
+};
+
 export default i18next;
